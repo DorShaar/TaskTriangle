76203fd4c3f40782befa7ac36090439877e0111b
Dor <Dor.Shaar@votiro.com>
2020-09-28 23:02:06 +0300

    Make Content and Resource be Json serializable

--- TaskTriangle/Content/TaskContent.cs
+++ TaskTriangle/Content/TaskContent.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Triangle.Content
 {
+    [JsonObject(MemberSerialization.OptIn)]
     public class TaskContent
     {
+        [JsonProperty]
         private readonly Dictionary<string, bool> mContents = new Dictionary<string, bool>();
 
         public IReadOnlyDictionary<string, bool> GetContents()
--- TaskTriangle/Resources/TaskResources.cs
+++ TaskTriangle/Resources/TaskResources.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Triangle.Resources
 {
+    [JsonObject(MemberSerialization.OptIn)]
     public class TaskResources
     {
+        [JsonProperty]
         private readonly List<string> mResources = new List<string>();
 
         public IEnumerable<string> GetResources()
